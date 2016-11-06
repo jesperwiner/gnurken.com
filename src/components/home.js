49b069b806eebbde3ec755b1befd5b4cb1971f9f
Jesper Winér <jesper.winer@gmail.com>
2016-11-06 23:04:53 +0100

    Added images

--- src/components/home.js
+++ src/components/home.js
@@ -29,5 +29,29 @@ export const Home = () => (
         </Row>
       </Col>
     </Row>
+
+    <Row xsAround className="latest-movies">
+      <Col xs={12}>
+        <Row xs>
+          <h3 className="latest-movies__text">
+            Latest World of Warcraft Movies
+          </h3>
+        </Row>
+        <Row xsAround>
+          <Col xsCenter className="player__col">
+            <div className="player__box">
+              <ReactPlayer url={`https://www.youtube.com/watch?v=${wowMovies[0].youTubeId}`} />
+              <h4>{`${wowMovies[0].name}`}</h4>
+            </div>
+          </Col>
+          <Col xsCenter className="player__col">
+            <div className="player__box">
+              <ReactPlayer url={`https://www.youtube.com/watch?v=${wowMovies[1].youTubeId}`} />
+              <h4>{`${wowMovies[1].name}`}</h4>
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   </div>
 );
